b0ab762d014a49e080a4d763699b73aa12fc8d62
pon-pn <75652042+pon-pn@users.noreply.github.com>
2022-12-30 17:15:44 +0900

    練習問題1

--- pro_01.js
+++ pro_01.js
@@ -1,1 +1,13 @@
 "use strict";
+/*
+const a = { a: 'a' }と
+const b = { b: 'b' }
+をマージしたcを出力してください
+e.g{ a:'a', b:'b' }
+*/
+
+const a = { a: "a" };
+const b = { b: "b" };
+const c = { ...a, ...b };
+
+console.log(c);
